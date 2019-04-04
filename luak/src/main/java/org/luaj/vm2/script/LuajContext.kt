f3d24c1dad147b153ecee1e0b81701929c784d9a
soywiz <soywiz@gmail.com>
2019-04-04 22:17:33 +0200

    Some automatic Java to Kotlin conversions + adjustments

---
+++ luak/src/main/java/org/luaj/vm2/script/LuajContext.kt
@@ -0,0 +1,141 @@
+/*******************************************************************************
+ * Copyright (c) 2013 LuaJ. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.luaj.vm2.script
+
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.PrintStream
+import java.io.Reader
+import java.io.Writer
+
+import javax.script.ScriptContext
+import javax.script.SimpleScriptContext
+
+import org.luaj.vm2.Globals
+import org.luaj.vm2.lib.jse.JsePlatform
+
+/**
+ * Context for LuaScriptEngine execution which maintains its own Globals,
+ * and manages the input and output redirection.
+ */
+class LuajContext
+/** Construct a LuajContext with its own globals, which
+ * which optionally are debug globals, and optionally use the
+ * luajc direct lua to java bytecode compiler.
+ *
+ *
+ * If createDebugGlobals is set, the globals
+ * created will be a debug globals that includes the debug
+ * library.  This may provide better stack traces, but may
+ * have negative impact on performance.
+ * @param createDebugGlobals true to create debug globals,
+ * false for standard globals.
+ * @param useLuaJCCompiler true to use the luajc compiler,
+ * reqwuires bcel to be on the class path.
+ */
+@JvmOverloads constructor(
+    createDebugGlobals: Boolean = "true" == System.getProperty("org.luaj.debug"),
+    useLuaJCCompiler: Boolean = "true" == System.getProperty("org.luaj.luajc")
+) : SimpleScriptContext(), ScriptContext {
+    init {
+        if (useLuaJCCompiler) {
+            throw RuntimeException("Can't use useLuaJCCompiler")
+        }
+    }
+
+    /** Globals for this context instance.  */
+    @JvmField
+    val globals: Globals = if (createDebugGlobals)
+        JsePlatform.debugGlobals()
+    else
+        JsePlatform.standardGlobals()
+
+    /** The initial value of globals.STDIN  */
+    private val stdin: InputStream = globals.STDIN
+    /** The initial value of globals.STDOUT  */
+    private val stdout: PrintStream = globals.STDOUT
+    /** The initial value of globals.STDERR  */
+    private val stderr: PrintStream = globals.STDERR
+
+    override fun setErrorWriter(writer: Writer?) {
+        globals.STDERR = if (writer != null)
+            PrintStream(WriterOutputStream(writer))
+        else
+            stderr
+    }
+
+    override fun setReader(reader: Reader?) {
+        globals.STDIN = reader?.let { ReaderInputStream(it) } ?: stdin
+    }
+
+    override fun setWriter(writer: Writer?) {
+        globals.STDOUT = if (writer != null)
+            PrintStream(WriterOutputStream(writer), true)
+        else
+            stdout
+    }
+
+    internal class WriterOutputStream(val w: Writer) : OutputStream() {
+        @Throws(IOException::class)
+        override fun write(b: Int) {
+            w.write(String(byteArrayOf(b.toByte())))
+        }
+
+        @Throws(IOException::class)
+        override fun write(b: ByteArray, o: Int, l: Int) {
+            w.write(String(b, o, l))
+        }
+
+        @Throws(IOException::class)
+        override fun write(b: ByteArray) {
+            w.write(String(b))
+        }
+
+        @Throws(IOException::class)
+        override fun close() {
+            w.close()
+        }
+
+        @Throws(IOException::class)
+        override fun flush() {
+            w.flush()
+        }
+    }
+
+    internal class ReaderInputStream(val r: Reader) : InputStream() {
+        @Throws(IOException::class)
+        override fun read(): Int {
+            return r.read()
+        }
+    }
+}
+/** Construct a LuajContext with its own globals which may
+ * be debug globals depending on the value of the system
+ * property 'org.luaj.debug'
+ *
+ *
+ * If the system property 'org.luaj.debug' is set, the globals
+ * created will be a debug globals that includes the debug
+ * library.  This may provide better stack traces, but may
+ * have negative impact on performance.
+ */
